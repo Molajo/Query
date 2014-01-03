38a680182487d0f464c277dba23586ee822be0d8
Amy Stephen <AmyStephen@gmail.com>
2014-01-03 16:34:40 -0600

    Copyright

--- .dev/Tests/MySQLi.php
+++ .dev/Tests/MySQLi.php
@@ -4,7 +4,7 @@
  *
  * @package    Molajo
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright  2013 Amy Stephen. All rights reserved.
+ * @copyright  2014 Amy Stephen. All rights reserved.
  */
 namespace Molajo\Query\Test;
 
@@ -16,7 +16,7 @@ use Molajo\Query\Handler\Joomla;
  *
  * @author     Amy Stephen
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
- * @copyright  2013 Amy Stephen. All rights reserved.
+ * @copyright  2014 Amy Stephen. All rights reserved.
  * @since      1.0
  */
 class JoomlaTest extends \PHPUnit_Framework_TestCase
