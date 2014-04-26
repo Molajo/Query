5a56d875ff23814473e00b67a9d163be9fcf1fb6
AmyStephen <AmyStephen@gmail.com>
2014-04-26 18:15:18 -0500

    autoload

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -11,8 +11,23 @@ include_once __DIR__ . '/CreateClassMap.php';
 $base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
 include $base . '/vendor/autoload.php';
 
-$results  = createClassMap($base . '/Source/Handler', 'Molajo\\Render\\Adapter\\');
-$classmap['Molajo\\Render\\Adapter'] = $base . '/Source/Adapter.php';
+$classmap = array();
+$results  = createClassMap($base . '/Controller', 'Molajo\\Controller\\');
+$classmap = array_merge($classmap, $results);
+$results  = createClassMap($base . '/Model', 'Molajo\\Model\\');
+$classmap = array_merge($classmap, $results);
+$results  = createClassMap($base . '/Resource/Adapter', 'Molajo\\Resource\\Adapter\\');
+$classmap = array_merge($classmap, $results);
+$results  = createClassMap($base . '/Resource/Api', 'Molajo\\Resource\\Api\\');
+$classmap = array_merge($classmap, $results);
+$results  = createClassMap($base . '/Resource/Configuration', 'Molajo\\Resource\\Configuration\\');
+$classmap = array_merge($classmap, $results);
+$results  = createClassMap($base . '/Resource/Factory', 'Molajo\\Resource\\Factory\\');
+$classmap = array_merge($classmap, $results);
+$results  = createClassMap($base . '/Source/Adapter', 'Molajo\\Query\\Adapter\\');
+$classmap = array_merge($classmap, $results);
+$classmap['Molajo\\Query\\Driver'] = $base . '/Source/Driver.php';
+
 ksort($classmap);
 
 spl_autoload_register(
